6110c2b98431b090125e1a29192c50169e11b560
edreeseg <edward.reeseg@gmail.com>
2019-05-06 16:16:05 -0400

    Creating Header component

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+const Header = props => (
+    <div className="header">
+        <h2>Realtime Chat App</h2>
+    </div>
+);
+
+export default Header;
